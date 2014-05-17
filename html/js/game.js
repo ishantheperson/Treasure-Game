c6023f7a5568259268b4d3b5727988948a236f7e
Ishan Bhargava <fireDude90@gmail.com>
2014-05-16 22:42:01 -0700

    can now connect to server

---
+++ html/js/game.js
@@ -0,0 +1,59 @@
+﻿var context;
+var images;
+var gameObjects = [];
+
+//#region Keyboard
+var keyboardState = {
+    leftDown: false,
+    rightDown: false
+};
+
+function keyDown(event) {
+    switch (event.keyCode) {
+        case 37: keyboardState.leftDown = true; break;
+        case 39: keyboardState.rightDown = true; break;
+    }
+}
+
+function keyUp(event) {
+    switch (event.keyCode) {
+        case 37: keyboardState.leftDown = false; break;
+        case 39: keyboardState.rightDown = false; break;
+    }
+}
+//#endregion
+
+function Dragon() {
+    this.x = 50;
+    this.y = 50;
+
+    this.speed = 2;
+
+    this.socket = io.connect('ws://localhost:8080');
+
+    this.draw = function () {
+        if (keyboardState.leftDown) { x -= speed; }
+        if (keyboardState.rightDown) { x += speed; }
+
+        context.drawImage(images.dragon, x, y);
+    }
+}
+
+function draw() {
+
+}
+
+$(document).ready(function () {
+    context = document.getElementById("game").getContext("2d");
+
+    images = {
+        dragon: document.getElementById("dragon")
+    };
+
+    $(document).keydown(keyDown);
+    $(document).keyup(keyUp);
+
+    gameObjects.push(new Dragon());
+
+    setInterval(draw, 10);
+});
